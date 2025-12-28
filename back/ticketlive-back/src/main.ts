7e5f052f571dec1bd43d22e7e8da4fb89a6f1f5f
Felipe RP <felipem.roserop@gmail.com>
2025-12-28 10:36:02 -0500

    adding swagger

--- back/ticketlive-back/src/main.ts
+++ back/ticketlive-back/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-import * as dotenv from "dotenv";
 import { LoggerMiddleware } from "./middlewares/logger.middleware";
-import cookieParser = require('cookie-parser');
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as dotenv from "dotenv";
+import cookieParser = require("cookie-parser");
 
 async function bootstrap() {
   dotenv.config();
@@ -11,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3005', 
+    origin: "http://localhost:3005",
     credentials: true,
   });
 
@@ -28,10 +29,19 @@ async function bootstrap() {
   //Usa mi midleware para registrar logs
   app.use(LoggerMiddleware);
 
+  //Coonfiguración documentación
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("TicketLive API")
+    .setDescription("API documentation")
+    .setVersion("1.0.0")
+    .addBearerAuth()
+    .build(); //Clase para documentacion OPENAPI 3.0
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("api", app, document);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
-
   console.log(`🚀 Server running on http://localhost:${port}`);
 }
-
 bootstrap();
